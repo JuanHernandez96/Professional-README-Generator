c1191c882e4b555ed6ef50524c4ba287421d3b8b
Juan Hernandez <juanh22@gmail.com>
2021-08-06 12:38:13 -0600

    added questions to index.js

---
+++ index.js
@@ -0,0 +1,98 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadMe = require('./Develop/utils/generateMarkdown');
+const { denied } = require('assert');
+
+
+
+const userInput = [
+    {
+        type: 'input',
+        name: 'ReadMe title',
+        message: "what's your Read Me title? (required)",
+        validate: Title => {
+            if (Title){
+                return true;
+            } else {
+                console.log('please enter the title of your Read Me')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Username',
+        message: "what's your GitHub Username? (required)",
+        validate: userGit => {
+            if (userGit) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username to continue')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What's yoru email?"
+    },
+    {
+        type: 'input',
+        name: 'Name of project',
+        message: "What's your Projects name? (required)",
+        validate: projectName => {
+            if (projectName) {
+                return true;
+            } else {
+                console.log("Please enter your projects name!")
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Description of project',
+        message: "what's your projects description",
+        validate: proDescription => {
+            if (proDescription) {
+                return true;
+            } else {
+                console.log('Please enter your projects description!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'licenseType',
+        message: "what license do you want to use? (use arrow keys to move up and down and space to select)",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+    },
+    {
+        type: 'input',
+        name: 'install command',
+        message: 'what command should run to install dependencies',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'information',
+        message: "what does the user need to know about this repository",
+    }
+]
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
